ff4c1c8673420e5792312b6cd6683ce819da686a
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-17 10:49:56 +0900

    v0.3 dectooct function with time_decorator

--- day01.py
+++ day01.py
@@ -1,23 +1,43 @@
-def factorial(number) -> int:
-    if number == 1:
-        return 1
+import time
+
+
+def time_decorator(func):
+    def init(*args):
+        s = time.time()
+        r = func(*args)
+        t = time.time()
+        print(f"걸린 시간 : {t - s}")
+        return r
+
+    return init
+
+
+def dectooct(number):
+    if number > 0:
+        return dectooct(number // 8) + f"{number % 8}"
     else:
-        return number * factorial(number - 1)
+        return ""
 
 
-def nCr(n, r) -> int:
-    '''
-    조합 함수
-    :param n:
-    :param r:
-    :return:
-    '''
-    numerator = factorial(n)
-    denominator = factorial(n-r) * factorial(r)
-    return numerator / denominator
+@time_decorator
+def dectooct2(number):
+    a = list()
+    while (number > 0):
+        a.append(number % 8)
+        number = number // 8
+
+    a.reverse()
+
+    return ''.join(map(str, a))
 
 
-if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {nCr(n, r)}")
+num = int(input("Input Number : "))
+
+# print(oct(num))
+
+s = time.time()
+print(dectooct(num))
+r = time.time()
+print(f"걸린 시간 : {r - s} \n")
+
+print(dectooct2(num))
