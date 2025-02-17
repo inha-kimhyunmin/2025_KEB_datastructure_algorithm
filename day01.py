c58326cb7ed50f2b63b37e990013776871d0e5d5
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-17 10:23:42 +0900

    v0.2 combination

--- day01.py
+++ day01.py
@@ -1,23 +1,23 @@
-def iseven(n):
-    """
-    짝수 판정 함수
-    :param n: 판정할 정수
-    :return: boolean
-    """
-    return not(n % 2)
-
-def iseven2(n):
-    """
-    짝수 판정 함수 2번재
-    :param n: 판정할 정수
-    :return: boolean
-    """
-    return not(n & 1)
-
-def isodd(n):
-    return bool(n & 1)
+def factorial(number) -> int:
+    if number == 1:
+        return 1
+    else:
+        return number * factorial(number - 1)
 
 
-for i in range(1,21):
-    print(f"{i}는 짝수인가? : {iseven(i)}, {iseven2(i)}")
-    print(f"{i}는 홀수인가? : {isodd(i)}\n")
+def nCr(n, r) -> int:
+    '''
+    조합 함수
+    :param n:
+    :param r:
+    :return:
+    '''
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    return numerator / denominator
+
+
+if __name__ == "__main__":
+    n = int(input("Input n : "))
+    r = int(input("Input r : "))
+    print(f"{n}C{r} = {nCr(n, r)}")
