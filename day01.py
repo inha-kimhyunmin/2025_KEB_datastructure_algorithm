8b33b74f3e475204c760c38d10a7a7e92f340409
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-17 11:35:59 +0900

    v0.4 sigma function O(n) = n

--- day01.py
+++ day01.py
@@ -1,43 +1,11 @@
-import time
 
-
-def time_decorator(func):
-    def init(*args):
-        s = time.time()
-        r = func(*args)
-        t = time.time()
-        print(f"걸린 시간 : {t - s}")
-        return r
-
-    return init
-
-
-def dectooct(number):
-    if number > 0:
-        return dectooct(number // 8) + f"{number % 8}"
-    else:
-        return ""
-
-
-@time_decorator
-def dectooct2(number):
-    a = list()
-    while (number > 0):
-        a.append(number % 8)
-        number = number // 8
-
-    a.reverse()
-
-    return ''.join(map(str, a))
-
-
+#O(n)
 num = int(input("Input Number : "))
+sum = 0
+for i in range(num + 1):
+    sum = sum + i
 
-# print(oct(num))
+print(sum)
 
-s = time.time()
-print(dectooct(num))
-r = time.time()
-print(f"걸린 시간 : {r - s} \n")
 
-print(dectooct2(num))
+print(num*(num+1)//2)
