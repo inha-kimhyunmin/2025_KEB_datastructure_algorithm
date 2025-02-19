22c1aa2a71c02beb5dd461b6ecbffd9e0d96f13b
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-19 10:41:59 +0900

    v3.2 day3 Tree with recursion

--- day03.py
+++ day03.py
@@ -1,31 +1,50 @@
 class TreeNode:
-	def __init__(self):
-		self.left = None
-		self.data = None
-		self.right = None
+    def __init__(self, data):
+        self.left = None
+        self.data = data
+        self.right = None
 
-node1 = TreeNode()
-node1.data = 'hs'
 
-node2 = TreeNode()
-node2.data = 'sl'
+def pre_order(node):
+    if node is None:
+        return
+    print(node.data, end=' -> ')
+    pre_order(node.left)
+    pre_order(node.right)
+
+
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.data, end = ' -> ')
+    in_order(node.right)
+
+
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node.data, end = ' -> ')
+
+
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node6 = TreeNode(6)
+
 node1.left = node2
-
-node3 = TreeNode()
-node3.data = 'mb'
 node1.right = node3
-
-node4 = TreeNode()
-node4.data = 'hw'
 node2.left = node4
-
-node5 = TreeNode()
-node5.data = 'zz'
 node2.right = node5
-
-node6 = TreeNode()
-node6.data = 'sm'
 node3.left = node6
 
-print(node6.data)
-print(node1.right.left.data)
+#        1
+#    2      3
+#  4   5   6
+#
+#
+pre_order(node1)
