cbaebbc2c3dc8fe18acbbcacb4cdfdf5835c5873
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-20 01:50:21 +0900

    v3.3

--- day03homework.py
+++ day03homework.py
@@ -32,7 +32,7 @@ def post_order(node):
     print(node.data, end=' -> ')
 
 
-def bstfind(root, find_group):
+def bst_find(root, find_group):
     current = root
     while True:
         if find_group == current.data:
@@ -51,20 +51,27 @@ def bstfind(root, find_group):
                 print(f"{find_group}이(가) 존재하지 않습니다")
                 break
 
-def findpredcessor(root, max, current):
-    if current is None:
-        return None
-    elif current.left is None and current.right is None: #리프 노드에서 min과 max 사이의 수 아무거나 찾기
+
+def find_predcessor(root, max, current):
+    """
+    자식 노드가 리프 노드가 아닐 때 현재 값에 들어갈 수 있는 최대값을 리프 노드에서 찾는 코드
+    :param root: TreeNode
+    :param max: current.right.data
+    :param current: TreeNode
+    :return: data
+    """
+    if current.left is None and current.right is None:  # 리프 노드에서 min과 max 사이의 수 아무거나 찾기
         if current.data < max:
             return current.data
         else:
             return None
-    right_result = findpredcessor(root, max, current.right)
+    right_result = find_predcessor(root, max, current.right)
     if right_result is not None:
         return right_result
-    left_result = findpredcessor(root, max, current.left)
+    left_result = find_predcessor(root, max, current.left)
     if left_result is not None:
         return left_result
+
 
 def ischildleaf(current):
     """
@@ -77,7 +84,8 @@ def ischildleaf(current):
     else:
         return False
 
-def bstdel(root, delete):
+
+def bst_delete(root, delete):
     current = root
     parent = None
     while current:
@@ -87,31 +95,25 @@ def bstdel(root, delete):
                     parent.left = None
                 else:  # 논리상 current가 parent의 왼쪽에도 없고 오른쪽에도 없는 경우는 없음
                     parent.right = None
-                break
             elif current.left != None and current.right == None:  # 왼쪽에 자식하나
                 if parent.left == current:
                     parent.left = current.left
                 else:
                     parent.right = current.left
-                break
             elif current.left == None and current.right != None:  # 오른쪽에 자식하나
                 if parent.left == current:
                     parent.left = current.right
                 else:
                     parent.right = current.right
-                break
             else:  # 자식이 둘 다 있는 경우 ->2일차 숙제
-                if ischildleaf(current): #자식 노드가 전부 리프 노드
+                if ischildleaf(current):  # 자식 노드가 전부 리프 노드
                     current.data = current.right.data
                     current.right = None
-                    break
-                else: #아닐 때
-                    insert = findpredcessor(root, current.right.data, current) #집어 넣을 리프 노드를 찾고
-                    bstdel(root, insert) #리프 노드를 제거함(찾는게 리프 노드이기때문에 무한루프에 안빠짐)
-                    current.data = insert #현재 data에 삽입
-                    break
-
-
+                else:  # 아닐 때
+                    insert = find_predcessor(root, current.right.data, current)  # 집어 넣을 리프 노드를 찾고
+                    bst_delete(root, insert)  # 리프 노드를 제거함(찾는게 리프 노드이기때문에 무한루프에 안빠짐)
+                    current.data = insert  # 현재 data에 삽입
+            break
         elif delete < current.data:
             if current.left == None:
                 print(f"{delete}이(가) 트리에 없습니다!")
@@ -126,16 +128,12 @@ def bstdel(root, delete):
             current = current.right
 
 
-if __name__ == "__main__":
-    # groups = random.sample(range(1,50), 10)
-    groups = [20, 10, 25, 28, 24, 8, 15, 27, 30, 23]
-    # groups = [20, 10, 23, 5, 16, 3, 8, 13, 17]
+def init_tree(groups):
+    global root
     root = None
-
     node = TreeNode(groups[0])
     root = node  # 초기화
 
-    # 트리 생성 코드
     for group in groups[1:]:
         node = TreeNode(group)
         current = root
@@ -146,7 +144,6 @@ if __name__ == "__main__":
                     break
                 else:
                     current = current.left
-
             else:
                 if current.right is None:
                     current.right = node
@@ -154,6 +151,15 @@ if __name__ == "__main__":
                 else:
                     current = current.right
 
+
+if __name__ == "__main__":
+    # groups = random.sample(range(1,50), 10)
+    groups = [20, 10, 25, 28, 24, 8, 15, 27, 30, 23]
+    # groups = [20, 10, 23, 5, 16, 3, 8, 13, 17]
+
+
+    root = None
+    init_tree(groups)
 
     # 탐색 코드
     # find_group = input("찾는 그룹명 입력 : ")
@@ -168,8 +174,8 @@ if __name__ == "__main__":
 
     # 제거 코드
     deleteName = 25
-    bstdel(root, deleteName)
-    print("제거 실행")
+    bst_delete(root, deleteName)
+    print(f"값 : {deleteName} 제거 실행")
     pre_order(root)
     print("End")
     in_order(root)
