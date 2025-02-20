9554614b810442132df5661f006eebf15e59279f
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-20 18:15:52 +0900

    v4.4 tree with bfs

--- day04homework.py
+++ day04homework.py
@@ -1,112 +1,203 @@
+import random
 from collections import deque
 
-class Graph:
-	def __init__(self, size) :
-		self.SIZE = size
-		self.graph = [[0 for _ in range(size)] for _ in range(size)]
+
+class TreeNode:
+    def __init__(self, data):
+        self.left = None
+        self.data = data
+        self.right = None
 
 
-def print_graph(g) :
-    print(' ', end = ' ')
-    for v in range(g.SIZE) :
-        print(name_ary[v], end =' ')
-    print()
-    for row in range(g.SIZE) :
-        print(name_ary[row], end =' ')
-        for col in range(g.SIZE) :
-            #print("%2d" % g.graph[row][col], end = ' ')
-            print(f"{g.graph[row][col]:2}", end=' ')
-        print()
-    print()
+def pre_order(node):
+    if node is None:
+        return
+    print(node.data, end=' -> ')
+    pre_order(node.left)
+    pre_order(node.right)
 
-def bfs(g, current, find_vtx):
-    visited = [0 for _ in range(g.SIZE)]
-    queue = deque([current])
-    visited[current] = 1
-    if visited[find_vtx] == 1:
-        return True
+
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.data, end=' -> ')
+    in_order(node.right)
+
+
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node.data, end=' -> ')
+
+
+def bfs_print(root : TreeNode):
+    queue = deque([root])
+
     while queue:
-        i = queue.popleft()
-        for vertex in range(g.SIZE):
-            if g.graph[i][vertex] != 0 and visited[vertex] == 0:
-                queue.append(vertex)
-                visited[vertex] = 1
-                if visited[find_vtx] == 1:
-                    return True
-    return False
+        node = queue.popleft()
+        print(node.data, end = ' -> ')
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
 
-def find_vertex_bfs(g, find_vtx):
-    return bfs(g, 0, find_vtx)
 
-def dfs(g, current, find_vtx, visited):
-    visited.append(current)
-    if current == find_vtx:
+
+def bst_find(root, find_group):
+    current = root
+    while True:
+        if find_group == current.data:
+            print(f"{find_group}이(가) 존재합니다")
+            break
+        elif find_group < current.data:
+            if current.left is not None:
+                current = current.left
+            else:
+                print(f"{find_group}이(가) 존재하지 않습니다")
+                break
+        else:
+            if current.right is not None:
+                current = current.right
+            else:
+                print(f"{find_group}이(가) 존재하지 않습니다")
+                break
+
+
+def find_predcessor(root, max, current):
+    """
+    자식 노드가 리프 노드가 아닐 때 현재 값에 들어갈 수 있는 최대값을 리프 노드에서 찾는 코드
+    :param root: TreeNode
+    :param max: current.right.data
+    :param current: TreeNode
+    :return: data
+    """
+    if current.left is None and current.right is None:  # 리프 노드에서 min과 max 사이의 수 아무거나 찾기
+        if current.data < max:
+            return current.data
+        else:
+            return None
+    right_result = find_predcessor(root, max, current.right)
+    if right_result is not None:
+        return right_result
+    left_result = find_predcessor(root, max, current.left)
+    if left_result is not None:
+        return left_result
+
+
+def ischildleaf(current):
+    """
+    자식 노드가 전부 리프노드인가?
+    :param current:
+    :return:
+    """
+    if current.left.left is None and current.left.right is None and current.right.left is None and current.right.left is None:
         return True
-    for vertex in range(g.SIZE):
-        if g.graph[current][vertex] != 0 and vertex not in visited:
-            if dfs(g, vertex, find_vtx, visited):
-                return True
-    return False
-
-def find_vertex(g, find_vtx) -> bool:
-    visited = []
-    return dfs(g, 0, find_vtx, visited)
+    else:
+        return False
 
 
-G1 = None
-name_ary = ['춘천', '서울', '속초', '대전', '광주', '부산']
-춘천, 서울, 속초, 대전, 광주, 부산 = 0, 1, 2, 3, 4, 5
+def bst_delete(root, delete):
+    current = root
+    parent = None
+    while current:
+        if delete == current.data:
+            if current.left == None and current.right == None:  # 자식이 없음
+                if parent.left == current:
+                    parent.left = None
+                else:  # 논리상 current가 parent의 왼쪽에도 없고 오른쪽에도 없는 경우는 없음
+                    parent.right = None
+            elif current.left != None and current.right == None:  # 왼쪽에 자식하나
+                if parent.left == current:
+                    parent.left = current.left
+                else:
+                    parent.right = current.left
+            elif current.left == None and current.right != None:  # 오른쪽에 자식하나
+                if parent.left == current:
+                    parent.left = current.right
+                else:
+                    parent.right = current.right
+            else:  # 자식이 둘 다 있는 경우 ->2일차 숙제
+                if ischildleaf(current):  # 자식 노드가 전부 리프 노드
+                    current.data = current.right.data
+                    current.right = None
+                else:  # 아닐 때
+                    insert = find_predcessor(root, current.right.data, current)  # 집어 넣을 리프 노드를 찾고
+                    bst_delete(root, insert)  # 리프 노드를 제거함(찾는게 리프 노드이기때문에 무한루프에 안빠짐)
+                    current.data = insert  # 현재 data에 삽입
+            break
+        elif delete < current.data:
+            if current.left == None:
+                print(f"{delete}이(가) 트리에 없습니다!")
+                break
+            parent = current
+            current = current.left
+        else:
+            if current.right == None:
+                print(f"{delete}이(가) 트리에 없습니다!")
+                break
+            parent = current
+            current = current.right
 
-g_size = 6
-G1 = Graph(g_size)
-G1.graph[춘천][서울] = 10; G1.graph[춘천][속초] = 15
-G1.graph[서울][춘천] = 10; G1.graph[서울][속초] = 40; G1.graph[서울][대전] = 11; G1.graph[서울][광주] = 50
-G1.graph[속초][춘천] = 15; G1.graph[속초][서울] = 40; G1.graph[속초][대전] = 12
-G1.graph[대전][서울] = 11; G1.graph[대전][속초] = 12; G1.graph[대전][광주] = 20; G1.graph[대전][부산] = 30
-G1.graph[광주][서울] = 50; G1.graph[광주][대전] = 20; G1.graph[광주][부산] = 25
-G1.graph[부산][대전] = 30; G1.graph[부산][광주] = 25
 
-print_graph(G1)
+def init_tree(groups):
+    global root
+    root = None
+    node = TreeNode(groups[0])
+    root = node  # 초기화
 
-# 간선 목록 만들기 [가중치, 시작도시, 도착도시]
-edge_ary = []
-for i in range(g_size) :
-	for k in range(g_size) :
-		if G1.graph[i][k] != 0 :
-			edge_ary.append([G1.graph[i][k], i, k])
+    for group in groups[1:]:
+        node = TreeNode(group)
+        current = root
+        while True:
+            if node.data < current.data:
+                if current.left is None:
+                    current.left = node
+                    break
+                else:
+                    current = current.left
+            else:
+                if current.right is None:
+                    current.right = node
+                    break
+                else:
+                    current = current.right
 
-print(edge_ary, len(edge_ary))
 
-# 가중치 순으로 목록 정렬 (내림차순)
-from operator import itemgetter
-edge_ary = sorted(edge_ary, key = itemgetter(0), reverse = True)
+def insert(current, data):
+    if current is None:
+        node = TreeNode(data)
+        return node
+    elif data < current.data:
+        current.left = insert(current.left, data)
+    else:
+        current.right = insert(current.right, data)
+    return current
 
-print(edge_ary, len(edge_ary))
 
-# 중복 간선 제거
-new_ary = []
-for i in range(0, len(edge_ary), 2) :
-	new_ary.append(edge_ary[i])
+if __name__ == "__main__":
+    # groups = random.sample(range(1,50), 10)
+    groups = [20, 10, 25, 28, 24, 8, 15, 27, 30, 23]
+    # groups = [20, 10, 23, 5, 16, 3, 8, 13, 17]
 
-print(new_ary, len(new_ary))
+    root = None
+    # 삽입 코드
+    # init_tree(groups)
+    for group in groups:
+        root = insert(root, group)
 
-index = 0
-while len(new_ary) > g_size - 1:	# 간선의 개수가 '정점 개수-1'일 때까지 반복
-	start = new_ary[index][1] #정렬되어있으니 가장 비용이 큰 간선
-	end = new_ary[index][2]
-	saveCost = new_ary[index][0] #가중치 저장
+    # 탐색 코드
+    # find_group = input("찾는 그룹명 입력 : ")
+    # bstfind(current, find_group)
 
-	G1.graph[start][end] = 0
-	G1.graph[end][start] = 0
+    bfs_print(root)
+    print('End')
 
-	startYN = find_vertex_bfs(G1, start)
-	endYN = find_vertex_bfs(G1, end)
-
-	if startYN and endYN :
-		del new_ary[index]
-	else :
-		G1.graph[start][end] = saveCost
-		G1.graph[end][start] = saveCost
-		index += 1
-
-print_graph(G1)
+    # 제거 코드
+    deleteName = 25
+    bst_delete(root, deleteName)
+    print(f"값 : {deleteName} 제거 실행")
+    bfs_print(root)
+    print('End')
